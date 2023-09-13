923e1ff2a501fd5f49ab18f4fc9363d4881d0464
HarryWang2222 <2774272087@qq.com>
2023-09-13 19:30:53 +0800

    txt_ combine refined

--- sci-hub-crawler-master/for IDM/txt_combine.py
+++ sci-hub-crawler-master/for IDM/txt_combine.py
@@ -1,12 +1,16 @@
+import re
+
 with open('./data/sm content.txt', 'r',encoding='utf-8')as content_file:
     for content_line in content_file.readlines():
         content_line = content_line[:-1]
-        year = int(content_line[0:4])
-        start = int(content_line[5:7])
-        end = int(content_line[8:10])
-        with open('./urls/{}.txt'.format(year), 'w',encoding='utf-8')as output:
+        if len(content_line) < 4:
+            break
+        content_split = re.split('\s|-', content_line)
+        year = int(content_split[0])
+        start = int(content_split[1])
+        end = int(content_split[2])
+        with open('./urls/{}.txt'.format(year), 'w', encoding='utf-8')as output:
             for i in range(start, end+1):
                 with open('./urls/sm{}.txt'.format(i), 'rt',encoding='utf-8')as sigle_file:
                     print(sigle_file.read(), file=output)
-
-
+                    print('test', file=output)
