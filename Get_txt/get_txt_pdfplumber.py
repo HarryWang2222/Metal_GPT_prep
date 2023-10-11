b29dcaa80aba84b57e5f8c81883ea734374848b4
HarryWang2222 <2774272087@qq.com>
2023-10-11 14:38:30 +0800

    test of pdfplumber
    failed to extract tabeles

--- Get_txt/get_txt_pdfplumber.py
+++ Get_txt/get_txt_pdfplumber.py
@@ -1,9 +1,32 @@
 import pdfplumber
 
+
+def not_within_bboxes(obj):
+    """Check if the object is in any of the table's bbox."""
+
+    def obj_in_bbox(_bbox):
+        """See https://github.com/jsvine/pdfplumber/blob/stable/pdfplumber/table.py#L404"""
+        v_mid = (obj["top"] + obj["bottom"]) / 2
+        h_mid = (obj["x0"] + obj["x1"]) / 2
+        x0, top, x1, bottom = _bbox
+        return (h_mid >= x0) and (h_mid < x1) and (v_mid >= top) and (v_mid < bottom)
+
+    return not any(obj_in_bbox(__bbox) for __bbox in bboxes)
+
+
 with pdfplumber.open("./data/test2.pdf") as pdf:
-    with open('./output/out_plumber2.txt', 'w', encoding='utf-8') as output:
-        for i in range(0,len(pdf.pages)):
-            page = pdf.pages[i]
-            print(page.find_tables(table_settings={}))
-            # txt = page.extract_text_simple(x_tolerance=3, y_tolerance=3)
-            # print(txt, file=output)
+    for page in pdf.pages:
+        bboxes = [
+            table.bbox
+            for table in page.find_tables(
+                table_settings={
+                    "vertical_strategy": "text",
+                    "horizontal_strategy": "lines",
+                    "explicit_vertical_lines": page.curves + page.edges,
+                    "explicit_horizontal_lines": page.curves + page.edges,
+                }
+            )
+        ]
+
+        print("\n\n\n\n\nText outside the tables:")
+        print(page.filter(not_within_bboxes).extract_text())
