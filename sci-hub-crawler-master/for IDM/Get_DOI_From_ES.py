c6e5a477492bc9785c2138ac1264a63dafbf72a7
HarryWang2222 <2774272087@qq.com>
2023-10-12 21:23:17 +0800

    all journal completed

--- sci-hub-crawler-master/for IDM/Get_DOI_From_ES.py
+++ sci-hub-crawler-master/for IDM/Get_DOI_From_ES.py
@@ -1,11 +1,16 @@
-for i in range(1,285):
-    with open ('./data_3/ijp{}.txt'.format(i), 'rt', encoding='utf-8')as file_read:
-        with open('./urls/ijp{}.txt'.format(i), 'w')as file_write:
+for i in range(1,87):
+    with open ('./data_4/acm{}.txt'.format(i), 'rt', encoding='utf-8')as file_read:
+        with open('./urls/acm{}.txt'.format(i), 'w')as file_write:
             for line in file_read.readlines():
                 if line.startswith('https'):
                     line = line.lower()
                     if line.find('(') != -1:
+                        '''
                         print('https://sci.bban.top/pdf/' + line[16:34] + '%2528' + line[35:37] + '%2529' + line[-9:-2] + '.pdf?download=true',
+                              file=file_write)
+                        '''
+                        # for acta metallurgica and materialia
+                        print('https://sci.bban.top/pdf/' + line[16:33] + '%2528' + line[34:36] + '%2529' + line[-9:-2] + '.pdf?download=true',
                               file=file_write)
                     else:
                         print('https://sci.bban.top/pdf/' + line[16:-2] + '.pdf?download=true', file=file_write)
@@ -14,3 +19,6 @@ for i in range(1,285):
 #https://doi.org/10.1016/j.pmatsci.2020.100671.
 #https://doi.org/10.1016/S0079-6425(20)30043-8.
 #https://sci.bban.top/pdf/10.1016/s0925-8388%252803%252900420-1.pdf?download=true
+
+#acta metallurgica and materialia
+#https://doi.org/10.1016/0956-7151(95)00132-F.
