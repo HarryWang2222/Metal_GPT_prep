9a2826c8f7112faf8976e2ab122f9a0d6f2b26a3
HarryWang2222 <2774272087@qq.com>
2023-10-13 21:50:41 +0800

    adding Philosophical Magazine

--- sci-hub-crawler-master/for IDM/Get_DOI_From_TS.py
+++ sci-hub-crawler-master/for IDM/Get_DOI_From_TS.py
@@ -1,20 +1,44 @@
-for i in range(1,6):
+import pandas as pd
+
+names = locals()
+for num in range(2000, 2022):
+    names['doi_list_%s' % num] = list()
+
+for i in range(1,5):
+    data_read = pd.read_csv('./data_5/pm{}.csv'.format(i))
+    year_list = data_read['Volume year'].values
+    csv_doi_list = data_read['DOI'].values
+    for ind in range(0, len(year_list)):
+        year = year_list[ind]
+        if year in range(2000,2022):
+            names['doi_list_%s' % year].append(csv_doi_list[ind])
+        else:
+            continue
+
+for year in range(2000, 2022):
+    with open('./pm_urls/pm{}.txt'.format(year), 'w', encoding='utf-8')as file_write:
+        for line in names['doi_list_%s' % year]:
+            print('https://sci.bban.top/pdf/' + str(line) + '.pdf', file=file_write)
+
+
+
+
+
+
+'''
+
     with open ('./data_4/acmm{}.txt'.format(i), 'rt', encoding='utf-8')as file_read:
         with open('./urls/acmm{}.txt'.format(i), 'w')as file_write:
             for line in file_read.readlines():
                 if line.startswith('https'):
                     line = line.lower()    #acmm1990-1994 do not need lower
                     if line.find('(') != -1:
-                        '''
-                        print('https://sci.bban.top/pdf/' + line[16:34] + '%2528' + line[35:37] + '%2529' + line[-9:-2] + '.pdf?download=true',
-                              file=file_write)
-                        '''
                         # for acta metallurgica and materialia
                         print('https://sci.bban.top/pdf/' + line[16:33] + '%2528' + line[34:36] + '%2529' + line[-9:-2] + '.pdf?download=true',
                               file=file_write)
                     else:
                         print('https://sci.bban.top/pdf/' + line[16:-2] + '.pdf?download=true', file=file_write)
-
+'''
 #10.1016/j.jallcom.2019.153073
 #https://doi.org/10.1016/j.pmatsci.2020.100671.
 #https://doi.org/10.1016/S0079-6425(20)30043-8.
--- sci-hub-crawler-master/for IDM/txt_combine.py
+++ sci-hub-crawler-master/for IDM/txt_combine.py
@@ -1,6 +1,6 @@
 import re
 
-with open('./data_4/acm content.txt', 'r',encoding='utf-8')as content_file:
+with open('./data_4/acmm content.txt', 'r',encoding='utf-8')as content_file:
     for content_line in content_file.readlines():
         content_line = content_line[:-1]
         if len(content_line) < 4:
@@ -9,9 +9,9 @@ with open('./data_4/acm content.txt', 'r',encoding='utf-8')as content_file:
         year = int(content_split[0])
         start = int(content_split[1])
         end = int(content_split[2])
-        with open('./urls/acm{}.txt'.format(year), 'w', encoding='utf-8')as output:
+        with open('./urls/acmm{}.txt'.format(year), 'w', encoding='utf-8')as output:
             for i in range(start, end+1):
-                with open('./urls/acm{}.txt'.format(i), 'rt',encoding='utf-8')as sigle_file:
+                with open('./urls/acmm{}.txt'.format(i), 'rt',encoding='utf-8')as sigle_file:
                     print(sigle_file.read(), file=output)
 
 #注意路径！！！替换期刊时所有路径都要改
