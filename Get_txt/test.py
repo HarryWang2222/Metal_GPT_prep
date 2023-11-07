723148ab0100e2210dddd238a81381e1f9ef60fd
HarryWang2222 <2774272087@qq.com>
2023-11-07 19:47:24 +0800

    prompts in test.py

--- Get_txt/test.py
+++ Get_txt/test.py
@@ -1,6 +1,80 @@
-from tqdm import tqdm
-import time
-
-for task in tqdm(range(100), leave=True):
-    print('\n')
-    time.sleep(0.1)
+{
+    "instruction": "Is iron the main element of steel?",
+    "input": ""
+},
+{
+    "instruction": "Is steel a kind of high entropy alloy?",
+    "input": ""
+},
+{
+    "instruction": "Give the common element composition of steel.",
+    "input": ""
+},
+{
+    "instruction": "Summarize the composition characteristics of high entropy alloys.",
+    "input": ""
+},
+{
+    "instruction": "What is the role of adding aluminum in nickel-based superalloys?",
+    "input": ""
+},
+{
+    "instruction": "Which one is the lattice structure of austenite in steel?",
+    "input": "BCC, FCC, HCP"
+},
+{
+    "instruction": "Is long-range order the structural feature of amorphous alloys?",
+    "input": ""
+},
+{
+    "instruction": "List the names of five common phases in steel.",
+    "input": ""
+},
+{
+    "instruction": "Summarize the characteristics of BCC structure.",
+    "input": ""
+},
+{
+    "instruction": "Which solid solution of A3B and A2B is more likely to form ordered solid solution according to the following conditions?",
+    "input": "Component A has a face-centered cubic crystal structure, and component B is dissolved in A to form a substitutional solid solution. "
+},
+{
+    "instruction": "Which one is not a kind of heat treatment?",
+    "input": "annealing, quenching, cold rolling, tempering"
+},
+{
+    "instruction": "Can quenching improve the hardness of alloys?",
+    "input": ""
+},
+{
+    "instruction": "What is the purpose of annealing?",
+    "input": ""
+},
+{
+    "instruction": "Summarize three commonly used cold processing methods",
+    "input": ""
+},
+{
+    "instruction": "Design a heat treatment process according to the following requirement.",
+    "input": "Improve the comprehensive mechanical properties of the alloy after rolling."
+},
+{
+    "instruction": "Is EBSD a method to characterize crystal orientation?",
+    "input": ""
+},
+{
+    "instruction": "Which of the following test methods cannot characterize the crystal structure?",
+    "input": "XRD, TEM, DSC"
+},
+{
+    "instruction": "List five mechanical performance indexes of metallic materials",
+    "input": ""
+},
+{
+    "instruction": "Summarize the characteristics of titanium alloy.",
+    "input": ""
+},
+{
+    "instruction": "Select appropriate characterization methods according to the following requirements.",
+    "input": "Analyzing phase structure and orientation."
+}
